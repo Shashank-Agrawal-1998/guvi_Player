143de6261843dcd65015ea3c76a7b5cb873063ef
Shashank-Agrawal-1998 <44235590+Shashank-Agrawal-1998@users.noreply.github.com>
2019-02-21 00:53:06 +0530

    017 Guvi Program

---
+++ 017.py
@@ -0,0 +1,16 @@
+def gcd(n,m):
+    if n==0:
+        return m
+    elif m==0:
+        return n
+    elif n%m==0:
+        return m
+    else:
+        return gcd(m,n%m)
+
+a,b = map(int, input().strip().split())
+if a>b:
+    result = (a*b)//gcd(a,b)
+else:
+    result = (a*b)//gcd(b,a)
+print(result)
