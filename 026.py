28c4f3a8e930fb2e00720af505a2aabd1493fd1b
Shashank-Agrawal-1998 <44235590+Shashank-Agrawal-1998@users.noreply.github.com>
2019-02-21 00:56:28 +0530

    026 Guvi Program

---
+++ 026.py
@@ -0,0 +1,21 @@
+def quicksort(arr, f, l):
+    if f<l:
+        k = partition(arr, f, l)
+        quicksort(arr,f,k-1)
+        quicksort(arr,k+1,l)
+        
+def partition(arr, f, l):
+    i = f-1
+    pivot = arr[l]
+    for j in range(f,l):
+        if arr[j]<=pivot:
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[l] = arr[l], arr[i+1]
+    return i+1
+    
+n = int(input())
+arr = list(map(int, input().strip().split()))
+quicksort(arr, 0, n-1)
+for i in range(n):
+    print(arr[i], end=' ')
