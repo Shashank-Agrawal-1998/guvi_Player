d5ba42718a289fbad1136d5f83dd66e372da66f5
Shashank-Agrawal-1998 <44235590+Shashank-Agrawal-1998@users.noreply.github.com>
2019-02-21 00:55:44 +0530

    024 Guvi Program

---
+++ 024.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().strip().split()))
+flag = 0 
+for i in range(0, n-1):
+    for j in range(0, n-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j],arr[j+1] = arr[j+1],arr[j]
+            flag = 1
+    if flag==0:
+        break
+for i in range(n):
+    print(arr[i], end=' ')
