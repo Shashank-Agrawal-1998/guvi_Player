6eca72229e38609b2ec5849fe1c2e3f20cf408ee
Shashank-Agrawal-1998 <44235590+Shashank-Agrawal-1998@users.noreply.github.com>
2019-04-09 00:56:45 +0530

    Update 024.py

--- 024.py
+++ 024.py
@@ -1,12 +1,12 @@
-n = int(input())
+n1 = int(input())
 arr = list(map(int, input().strip().split()))
 flag = 0 
-for i in range(0, n-1):
-    for j in range(0, n-i-1):
+for i in range(0, n1-1):
+    for j in range(0, n1-i-1):
         if arr[j] > arr[j+1]:
             arr[j],arr[j+1] = arr[j+1],arr[j]
             flag = 1
     if flag==0:
         break
-for i in range(n):
+for i in range(n1):
     print(arr[i], end=' ')
